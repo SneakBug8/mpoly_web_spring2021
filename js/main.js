495aa3097a1beaa6fd46567c78a079de62223811
SneakBug8 <sneakbug8@gmail.com>
2021-07-09 09:27:02 +0300

    dark theme logic

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ new Vue({
     el: '#app',
     data() {
         return {
-
+            darkTheme: false,
         };
     },
     computed: {},
     mounted() {
+        if (localStorage.getItem('theme')) {
+            this.darkTheme = true && localStorage.getItem('theme');
+        }
     },
     watch: {
+        darkTheme: {
+            handler: function(u) {
+                localStorage.setItem('theme', (u * 1) + "");
+            },
+        }
     },
     computed: {
     },
